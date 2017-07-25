672f73111e58509dd70ee78c722ae9b4c7e1cf49
ZxPwd <stingymilk@gmail.com>
2017-07-25 04:52:13 -0400

    RestarterBot-master
    version 1.2

---
+++ BillardRestarter Bot/Form1.Designer.vb
@@ -0,0 +1,187 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.tmrStartBot = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrPause = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrWait = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrStartBot2 = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrStartBot3 = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrBsTv = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrWelcome = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrHotkey = New System.Windows.Forms.Timer(Me.components)
+        Me.FormSkin1 = New BillardRestarter_Bot.FormSkin()
+        Me.FlatMini1 = New BillardRestarter_Bot.FlatMini()
+        Me.FlatClose1 = New BillardRestarter_Bot.FlatClose()
+        Me.lblStatus = New System.Windows.Forms.Label()
+        Me.FlatListBox1 = New BillardRestarter_Bot.FlatListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.FormSkin1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'tmrStartBot
+        '
+        Me.tmrStartBot.Interval = 2000
+        '
+        'tmrPause
+        '
+        Me.tmrPause.Interval = 25000
+        '
+        'tmrWait
+        '
+        Me.tmrWait.Interval = 2000
+        '
+        'tmrStartBot2
+        '
+        '
+        'tmrStartBot3
+        '
+        Me.tmrStartBot3.Interval = 2000
+        '
+        'tmrBsTv
+        '
+        Me.tmrBsTv.Interval = 2500
+        '
+        'tmrWelcome
+        '
+        Me.tmrWelcome.Interval = 5000
+        '
+        'tmrHotkey
+        '
+        Me.tmrHotkey.Interval = 900
+        '
+        'FormSkin1
+        '
+        Me.FormSkin1.BackColor = System.Drawing.Color.White
+        Me.FormSkin1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(60, Byte), Integer), CType(CType(70, Byte), Integer), CType(CType(73, Byte), Integer))
+        Me.FormSkin1.BorderColor = System.Drawing.Color.FromArgb(CType(CType(53, Byte), Integer), CType(CType(58, Byte), Integer), CType(CType(60, Byte), Integer))
+        Me.FormSkin1.Controls.Add(Me.Label1)
+        Me.FormSkin1.Controls.Add(Me.FlatMini1)
+        Me.FormSkin1.Controls.Add(Me.FlatClose1)
+        Me.FormSkin1.Controls.Add(Me.lblStatus)
+        Me.FormSkin1.Controls.Add(Me.FlatListBox1)
+        Me.FormSkin1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FormSkin1.FlatColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.FormSkin1.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.FormSkin1.HeaderColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(47, Byte), Integer), CType(CType(49, Byte), Integer))
+        Me.FormSkin1.HeaderMaximize = False
+        Me.FormSkin1.Location = New System.Drawing.Point(0, 0)
+        Me.FormSkin1.Name = "FormSkin1"
+        Me.FormSkin1.Size = New System.Drawing.Size(297, 488)
+        Me.FormSkin1.TabIndex = 0
+        Me.FormSkin1.Text = "Billard RestarterBot"
+        '
+        'FlatMini1
+        '
+        Me.FlatMini1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.FlatMini1.BackColor = System.Drawing.Color.White
+        Me.FlatMini1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(47, Byte), Integer), CType(CType(49, Byte), Integer))
+        Me.FlatMini1.Font = New System.Drawing.Font("Marlett", 12.0!)
+        Me.FlatMini1.Location = New System.Drawing.Point(251, 16)
+        Me.FlatMini1.Name = "FlatMini1"
+        Me.FlatMini1.Size = New System.Drawing.Size(18, 18)
+        Me.FlatMini1.TabIndex = 6
+        Me.FlatMini1.Text = "FlatMini1"
+        Me.FlatMini1.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        '
+        'FlatClose1
+        '
+        Me.FlatClose1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.FlatClose1.BackColor = System.Drawing.Color.White
+        Me.FlatClose1.BaseColor = System.Drawing.Color.FromArgb(CType(CType(168, Byte), Integer), CType(CType(35, Byte), Integer), CType(CType(35, Byte), Integer))
+        Me.FlatClose1.Font = New System.Drawing.Font("Marlett", 10.0!)
+        Me.FlatClose1.Location = New System.Drawing.Point(275, 16)
+        Me.FlatClose1.Name = "FlatClose1"
+        Me.FlatClose1.Size = New System.Drawing.Size(18, 18)
+        Me.FlatClose1.TabIndex = 5
+        Me.FlatClose1.Text = "FlatClose1"
+        Me.FlatClose1.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        '
+        'lblStatus
+        '
+        Me.lblStatus.AutoSize = True
+        Me.lblStatus.BackColor = System.Drawing.Color.Transparent
+        Me.lblStatus.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblStatus.ForeColor = System.Drawing.SystemColors.ActiveCaption
+        Me.lblStatus.Location = New System.Drawing.Point(6, 52)
+        Me.lblStatus.Name = "lblStatus"
+        Me.lblStatus.Size = New System.Drawing.Size(63, 16)
+        Me.lblStatus.TabIndex = 4
+        Me.lblStatus.Text = "Waiting..."
+        '
+        'FlatListBox1
+        '
+        Me.FlatListBox1.BackColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(47, Byte), Integer), CType(CType(49, Byte), Integer))
+        Me.FlatListBox1.items = New String() {""}
+        Me.FlatListBox1.Location = New System.Drawing.Point(3, 90)
+        Me.FlatListBox1.Name = "FlatListBox1"
+        Me.FlatListBox1.SelectedColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.FlatListBox1.Size = New System.Drawing.Size(290, 395)
+        Me.FlatListBox1.TabIndex = 0
+        Me.FlatListBox1.Text = "FlatListBox1"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.YellowGreen
+        Me.Label1.Location = New System.Drawing.Point(6, 68)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(78, 16)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Version: 1.2"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(297, 488)
+        Me.Controls.Add(Me.FormSkin1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.FormSkin1.ResumeLayout(False)
+        Me.FormSkin1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents FormSkin1 As FormSkin
+    Friend WithEvents FlatListBox1 As FlatListBox
+    Friend WithEvents lblStatus As Label
+    Friend WithEvents FlatClose1 As FlatClose
+    Friend WithEvents FlatMini1 As FlatMini
+    Friend WithEvents tmrStartBot As Timer
+    Friend WithEvents tmrPause As Timer
+    Friend WithEvents tmrWait As Timer
+    Friend WithEvents tmrStartBot2 As Timer
+    Friend WithEvents tmrStartBot3 As Timer
+    Friend WithEvents tmrBsTv As Timer
+    Friend WithEvents tmrWelcome As Timer
+    Friend WithEvents tmrHotkey As Timer
+    Friend WithEvents Label1 As Label
+End Class
---
+++ BillardRestarter Bot/Form1.vb
@@ -0,0 +1,276 @@
+﻿Imports System.Text
+Imports System.Net
+Imports System.IO
+'Imports System.Web.Script.Serialization
+Imports BitmapDetector2.Search_Image
+Imports System.ComponentModel
+Imports System.Security.Cryptography
+Imports System.Threading
+Imports System.Runtime.InteropServices
+Imports System.Collections
+Imports System
+Public Class Form1
+    Dim user_ As String
+    Dim pass_ As String
+    Dim region_ As String
+    Dim lolpath_ As String
+    Public version As String
+    Public upurl As String
+    Public upavail As Boolean
+
+
+
+
+#Region "Section - Declarations"
+    Private Declare Sub mouse_event Lib "user32.dll" (ByVal dwFlags As Integer, ByVal dx As Integer, ByVal dy As Integer, ByVal cButtons As Integer, ByVal dwExtraInfo As IntPtr)
+
+
+
+    Public Declare Function GetKeyPress Lib "user32" Alias "GetAsyncKeyState" (ByVal key As Integer) As Integer
+    Private Declare Function GetAsyncKeyState Lib "user32" (ByVal vkey As Integer) As Short
+    Private Const mouseclickup = 4
+    Private Const mouseclickdown = 2
+    Public progress As Integer = 0
+    Public countdown As Integer = 0
+    Private Declare Function GetAsyncKeyState Lib "user32" (ByVal vkey As Long) As Integer
+    Declare Sub mouse_event Lib "user32" Alias "mouse_event" (ByVal dwFlags As Integer, ByVal dx As Integer, ByVal dy As Integer, ByVal cButtons As Integer, ByVal dwExtraInfo As Integer)
+    Private Const MOUSEEVENTF_ABSOLUTE = &H8000 ' absolute move
+    Private Const MOUSEEVENTF_LEFTDOWN = &H2 ' left button down
+    Private Const MOUSEEVENTF_LEFTUP = &H4 ' left button up
+    Private Const MOUSEEVENTF_MOVE = &H1 ' mouse move
+    Private Const MOUSEEVENTF_MIDDLEDOWN = &H20
+    Private Const MOUSEEVENTF_MIDDLEUP = &H40
+    Private Const MOUSEEVENTF_RIGHTDOWN = &H8
+    Private Const MOUSEEVENTF_RIGHTUP = &H10
+    Declare Function SetCursorPos& Lib "user32" (ByVal p As Point)
+
+
+
+
+
+#End Region
+
+#Region "Section - Functions & Subs"
+
+    Private Sub PerformMouseClick(ByVal LClick_RClick_DClick As String)
+        Const MOUSEEVENTF_LEFTDOWN As Integer = 2
+        Const MOUSEEVENTF_LEFTUP As Integer = 4
+        Const MOUSEEVENTF_RIGHTDOWN As Integer = 6
+        Const MOUSEEVENTF_RIGHTUP As Integer = 8
+        If LClick_RClick_DClick = "RClick" Then
+            mouse_event(MOUSEEVENTF_RIGHTDOWN, 0, 0, 0, IntPtr.Zero)
+            mouse_event(MOUSEEVENTF_RIGHTUP, 0, 0, 0, IntPtr.Zero)
+        ElseIf LClick_RClick_DClick = "LClick" Then
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, IntPtr.Zero)
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, IntPtr.Zero)
+        ElseIf LClick_RClick_DClick = "DClick" Then
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, IntPtr.Zero)
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, IntPtr.Zero)
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, IntPtr.Zero)
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, IntPtr.Zero)
+        End If
+    End Sub
+    Private Function PressButton(ByRef image As Bitmap, ByRef press As Boolean, ByRef x As Integer, ByRef y As Integer)
+        Dim bounds As Rectangle
+        Dim point As New Point
+        Dim screenshot As System.Drawing.Bitmap
+        Dim graph As Graphics
+        bounds = Screen.PrimaryScreen.Bounds
+        screenshot = New System.Drawing.Bitmap(bounds.Width, bounds.Height, System.Drawing.Imaging.PixelFormat.Format32bppRgb)
+        graph = Graphics.FromImage(screenshot)
+        graph.CopyFromScreen(0, 0, 0, 0, bounds.Size, CopyPixelOperation.SourceCopy)
+        Dim returnString As Point = BitmapDetector2.Search_Image.search(screenshot, image, 0)
+        If returnString.X = 0 And returnString.Y = 0 Then
+            Return False
+        Else
+
+            If press = True Then
+                point.X = returnString.X + x
+                point.Y = returnString.Y + y
+                Windows.Forms.Cursor.Position = point
+                PerformMouseClick("LClick")
+
+            Else
+                point.X = returnString.X + x
+                point.Y = returnString.Y + y
+                Windows.Forms.Cursor.Position = point
+            End If
+
+            Return True
+        End If
+    End Function
+    Sub status(ByVal status As String)
+        lblStatus.Text = status & " @ " & My.Computer.Clock.LocalTime
+    End Sub
+#End Region
+
+#Region "Section - Timers"
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles tmrStartBot.Tick
+
+        tmrStartBot.Interval = 2000
+        If PressButton(My.Resources.android_inactive, True, 10, 10) Then
+            FlatListBox1.AddItem("InactiveTab Found!")
+            PerformMouseClick("LClick")
+            PerformMouseClick("LClick")
+            FlatListBox1.AddItem("Waiting 25 Seconds for android home")
+            FlatListBox1.AddItem("STOPPED: tmrStartBot")
+            FlatListBox1.AddItem("STARTED: tmrPause Enabled = True")
+            tmrStartBot.Stop()
+            tmrPause.Enabled = True
+        Else
+            status("Searching for InactiveTab")
+        End If
+
+
+    End Sub
+
+    Private Sub tmrPause_Tick(sender As Object, e As EventArgs) Handles tmrPause.Tick
+        tmrPause.Enabled = False
+        FlatListBox1.AddItem("25 Second wait done!")
+        FlatListBox1.AddItem("STOPPED: tmrPause Enabled = False")
+        FlatListBox1.AddItem("STARTED: tmrStartBot2")
+        status("TmrPause")
+        tmrStartBot2.Start()
+    End Sub
+    Private Sub tmrWelcome_Tick(sender As Object, e As EventArgs) Handles tmrWelcome.Tick
+        tmrWelcome.Enabled = False
+        status("Started RestarterBot")
+        tmrWelcome.Enabled = False
+        Dim x As Integer
+        Dim y As Integer
+        x = Screen.PrimaryScreen.WorkingArea.Width - 300
+        y = Screen.PrimaryScreen.WorkingArea.Height - 500
+        Me.Location = New Point(x, y)
+
+        'For Each p5 As Process In Process.GetProcesses()
+        '    If p5.ProcessName = "BlueStacks_old" Then 'Or p.ProcessName = "mspaint"
+        '        p5.Kill()
+        '        FlatListBox1.AddItem("TERMINATED: BlueStacks_old.exe")
+        '    Else
+        '        Process.Start("C:\ProgramData\BlueStacksGameManager\BlueStacks_old.exe")
+
+        '    End If
+        'Next
+        For Each p5 As Process In Process.GetProcesses()
+            If p5.ProcessName = "BlueStacks_old" Then 'Or p.ProcessName = "mspaint"
+                p5.Kill()
+                FlatListBox1.AddItem("TERMINATED: BlueStacks_old.exe")
+
+            ElseIf p5.ProcessName = "BlueStacks_old" Then 'Or p.ProcessName = "mspaint"
+                p5.Kill()
+                FlatListBox1.AddItem("TERMINATED: HD-RunApp.exe {NEW}")
+            Else
+                Process.Start("C:\ProgramData\BlueStacksGameManager\BlueStacks_old.exe")
+
+            End If
+
+        Next
+
+        tmrBsTv.Start()
+    End Sub
+    Private Sub tmrStartBot2_Tick_1(sender As Object, e As EventArgs) Handles tmrStartBot2.Tick
+        tmrStartBot2.Interval = 2000
+
+        If PressButton(My.Resources.billard_inactive, True, 10, 10) Or
+        PressButton(My.Resources.billard_active, True, 10, 10) Then
+            FlatListBox1.AddItem("Detected Android Home")
+
+            PerformMouseClick("LClick")
+            PerformMouseClick("LClick")
+            tmrWait.Enabled = True
+            FlatListBox1.AddItem("STARTED: tmrWait")
+            FlatListBox1.AddItem("Waiting 2 seconds")
+            FlatListBox1.AddItem("CONTINUE: tmrStartBot2 {NEW} v2")
+            'tmrStartBot2.Stop()
+
+        Else
+            status("TmrStartBot2")
+        End If
+
+
+    End Sub
+
+
+    Private Sub tmrWait_Tick(sender As Object, e As EventArgs) Handles tmrWait.Tick
+        tmrWait.Enabled = False
+        FlatListBox1.AddItem("2 Second wait finished")
+        FlatListBox1.AddItem("Waiting for Miniclip")
+        FlatListBox1.AddItem("STARTED: tmrStartBot3")
+        tmrStartBot3.Start()
+
+
+    End Sub
+
+    Private Sub tmrStartBot2_Tick(sender As Object, e As EventArgs) Handles tmrStartBot3.Tick
+        On Error Resume Next
+        tmrStartBot3.Interval = 2000
+        If PressButton(My.Resources.miniclip, True, 10, 10) Then
+            FlatListBox1.AddItem("Miniclip Detected")
+            Process.Start("BillardBot.exe")
+            End
+
+        Else
+            'Thread.Sleep(2000)
+            status("tmrStartBot2")
+        End If
+
+
+    End Sub
+
+    Private Sub tmrBsTv_Tick(sender As Object, e As EventArgs) Handles tmrBsTv.Tick
+        Thread.Sleep(2000)
+        tmrBsTv.Interval = 2000
+        If PressButton(My.Resources.bstvexit, True, 10, 10) Then
+            PerformMouseClick("LClick")
+            PerformMouseClick("LClick")
+            FlatListBox1.AddItem("TERIMINATED: BlueStacks TV ")
+            tmrBsTv.Stop()
+            FlatListBox1.AddItem("STARTED: tmrStartBot")
+            tmrStartBot.Start()
+        Else
+            status("TmrBsTv")
+        End If
+    End Sub
+
+    Dim hotkey0 As Boolean
+    Private Sub tmrHotkey_Tick(sender As Object, e As EventArgs) Handles tmrHotkey.Tick
+        If (GetAsyncKeyState(Keys.NumPad0) And 1) > 0 Then
+            hotkey0 = Not hotkey0
+            If hotkey0 Then
+                End
+
+            Else
+                End
+
+            End If
+        End If
+    End Sub
+
+#End Region
+
+
+    Private Sub FormSkin1_Click(sender As Object, e As EventArgs) Handles FormSkin1.Click
+
+    End Sub
+
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        tmrHotkey.Start()
+        FlatListBox1.AddItem("EXIT HOTKEY: Numpad + 0")
+        tmrWelcome.Enabled = True
+        FlatListBox1.AddItem("STARTING IN 5 SECONDS!")
+    End Sub
+
+
+
+    Private Sub FlatListBox1_Click(sender As Object, e As EventArgs) Handles FlatListBox1.Click
+
+    End Sub
+
+    Private Sub FlatClose1_Click(sender As Object, e As EventArgs) Handles FlatClose1.Click
+
+    End Sub
+
+
+End Class
